6847b8e0ee70a5231375432591fae34f16a7a084
Hemant Kumar <iamhemantindia@protonmail.com>
2024-02-18 21:08:42 +0530

    recieve share intent from youtube

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
 import 'dart:developer';
-import 'dart:io';
+import 'dart:io' as io;
+import 'package:Bloomee/model/MediaPlaylistModel.dart';
+import 'package:Bloomee/model/songModel.dart';
+import 'package:Bloomee/model/youtube_vid_model.dart';
+import 'package:Bloomee/repository/Youtube/youtube_api.dart';
 import 'package:Bloomee/screens/widgets/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,15 +17,69 @@ import 'package:Bloomee/screens/screen/library_views/cubit/current_playlist_cubi
 import 'package:Bloomee/screens/screen/library_views/cubit/import_playlist_cubit.dart';
 import 'package:Bloomee/services/db/cubit/mediadb_cubit.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'package:go_router/go_router.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 import 'blocs/mediaPlayer/bloomee_player_cubit.dart';
+
+bool isYoutubeLink(String link) {
+  if (link.contains("youtube.com") || link.contains("youtu.be")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+String? extractVideoId(String url) {
+  Uri uri = Uri.parse(url);
+
+  // Check if the URL is from youtube.com
+  if (uri.host == 'youtube.com') {
+    return uri.queryParameters['v']; // Retrieve video ID from query parameter
+  }
+
+  // Check if the URL is from youtu.be
+  if (uri.host == 'youtu.be') {
+    return uri.pathSegments.first; // Retrieve video ID from path
+  }
+
+  // Invalid URL format
+  return null;
+}
+
+void ProcessIncomingIntent(List<SharedMediaFile> _sharedFiles) {
+  if (Uri.tryParse(_sharedFiles[0].path) != null &&
+      isYoutubeLink(_sharedFiles[0].path)) {
+    var _tempId = extractVideoId(_sharedFiles[0].path);
+    if (_tempId != null) {
+      log(extractVideoId(_sharedFiles[0].path).toString(),
+          name: "Shared Files");
+      var _tempVid = YouTubeServices().getVideoFromId(_tempId);
+      _tempVid.then((value) {
+        if (value != null) {
+          YouTubeServices()
+              .formatVideo(video: value, quality: "High")
+              .then((value) {
+            if (value != null) {
+              MediaItemModel _mIM = fromYtVidSongMap2MediaItem(value);
+              MediaPlaylist _tempPlaylist = MediaPlaylist(
+                albumName: "Shared Playlist",
+                mediaItems: [_mIM],
+              );
+              log(_mIM.title, name: "Shared Files");
+            }
+          });
+        }
+      });
+    }
+  }
+}
 
 void main() {
   try {
     dotenv.load(fileName: "assets/.env");
   } on Exception catch (e) {
     log("error $e");
-    dotenv.load(mergeWith: Platform.environment);
+    dotenv.load(mergeWith: io.Platform.environment);
   }
   runApp(const MyApp());
 }
@@ -35,6 +94,33 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   // Initialize the player
   // This widget is the root of your application.
+  late StreamSubscription _intentSub;
+  final _sharedFiles = <SharedMediaFile>[];
+  @override
+  void initState() {
+    super.initState();
+
+    // For sharing or opening urls/text coming from outside the app while the app is in the memory
+    _intentSub = ReceiveSharingIntent.getMediaStream().listen((event) {
+      _sharedFiles.clear();
+      _sharedFiles.addAll(event);
+      log(_sharedFiles[0].mimeType.toString(), name: "Shared Files");
+      log(_sharedFiles[0].path, name: "Shared Files");
+      ProcessIncomingIntent(_sharedFiles);
+      // Tell the library that we are done processing the intent.
+      ReceiveSharingIntent.reset();
+    });
+
+    // For sharing or opening urls/text coming from outside the app while the app is closed
+    ReceiveSharingIntent.getInitialMedia().then((event) {
+      _sharedFiles.clear();
+      _sharedFiles.addAll(event);
+      log(_sharedFiles[0].mimeType.toString(), name: "Shared Files Offline");
+      log(_sharedFiles[0].path, name: "Shared Files");
+      ProcessIncomingIntent(_sharedFiles);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
