9b10106184b48cbc6a673f08ca1a06fa7adeef38
Hemant KArya <65885023+HemantKArya@users.noreply.github.com>
2024-03-25 22:56:28 +0530

    miniplayercubit removed

--- lib/screens/widgets/miniPlayerBloc/cubit/mini_player_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'mini_player_state.dart';
-
-class MiniPlayerCubit extends Cubit<bool> {
-  MiniPlayerCubit() : super(false);
-}
--- lib/screens/widgets/miniPlayerBloc/cubit/mini_player_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'mini_player_cubit.dart';
-
-@immutable
-sealed class MiniPlayerState {}
-
-final class MiniPlayerInitial extends MiniPlayerState {}
