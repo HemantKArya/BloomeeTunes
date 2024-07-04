a166a0ab87dc5b7dedf96826efdd8c60e09ec623
Hemant KArya <65885023+HemantKArya@users.noreply.github.com>
2024-07-04 15:49:04 +0530

    recently tab ui impr.

--- lib/screens/widgets/tabList_widget.dart
+++ lib/screens/widgets/tabList_widget.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:Bloomee/screens/widgets/paging_scroll.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import '../../theme_data/default.dart';
 
@@ -55,7 +56,9 @@ Widget buildColumnsCards(List<Widget> items, context, {int columnLength = 4}) {
   final cards = <Widget>[];
   Widget feautredCards;
   int endIndex = columnLength;
-  double itemWidth = (MediaQuery.of(context).size.width - 40) * 0.88;
+  double itemWidth = ResponsiveBreakpoints.of(context).smallerOrEqualTo(TABLET)
+      ? ((MediaQuery.of(context).size.width - 40) * 0.88)
+      : ((MediaQuery.of(context).size.width - 40) * 0.48);
   if (endIndex > items.length) endIndex = items.length;
   if (items.isNotEmpty) {
     for (int i = 0; i < items.length; i += columnLength) {
