56f673c8785bbc40f77a7bac599a87c0a368eeaf
Hemant KArya <65885023+HemantKArya@users.noreply.github.com>
2024-08-05 17:13:16 +0530

    url launcher set to external application for updates

--- lib/utils/url_launcher.dart
+++ lib/utils/url_launcher.dart
@@ -3,7 +3,7 @@ import 'dart:developer';
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launch_Url(_url) async {
-  if (!await launchUrl(_url)) {
+  if (!await launchUrl(_url, mode: LaunchMode.externalApplication)) {
     log('Could not launch $_url', name: "launch_Url");
   }
 }
