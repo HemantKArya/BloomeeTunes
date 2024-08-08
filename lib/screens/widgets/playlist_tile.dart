911dced50a8ad649a5b801b396f6912ebf6cf13d
Hemant KArya <65885023+HemantKArya@users.noreply.github.com>
2024-08-08 15:02:20 +0530

    playing indicator for playlist tile

--- lib/screens/widgets/playlist_tile.dart
+++ lib/screens/widgets/playlist_tile.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:Bloomee/blocs/mediaPlayer/bloomee_player_cubit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:Bloomee/theme_data/default.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class SmallPlaylistCard extends StatelessWidget {
   final String playListTitle;
@@ -24,6 +27,22 @@ class SmallPlaylistCard extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          StreamBuilder<String>(
+              stream:
+                  context.watch<BloomeePlayerCubit>().bloomeePlayer.queueTitle,
+              builder: (context, snapshot) {
+                if (snapshot.hasData && snapshot.data == playListTitle) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Icon(
+                      FontAwesome.chart_simple_solid,
+                      color: Default_Theme.primaryColor2.withOpacity(1),
+                      size: 15,
+                    ),
+                  );
+                }
+                return const SizedBox.shrink();
+              }),
           Padding(
             padding: const EdgeInsets.only(left: 10, right: 10),
             child: SizedBox.square(
