7de87baa33dd0336daa2d3d930bd9588c9da2c79
Hemant KArya <65885023+HemantKArya@users.noreply.github.com>
2024-08-18 13:24:21 +0530

    Listview horizontal -> new Flitter carousel

--- lib/screens/widgets/horizontal_card_view.dart
+++ lib/screens/widgets/horizontal_card_view.dart
@@ -1,16 +1,15 @@
 import 'package:Bloomee/blocs/mediaPlayer/bloomee_player_cubit.dart';
 import 'package:Bloomee/screens/screen/home_views/youtube_views/playlist.dart';
-import 'package:Bloomee/screens/widgets/paging_scroll.dart';
 import 'package:Bloomee/screens/widgets/square_card.dart';
 import 'package:Bloomee/theme_data/default.dart';
 import 'package:Bloomee/utils/external_list_importer.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HorizontalCardView extends StatelessWidget {
   final Map<String, dynamic> data;
-  const HorizontalCardView({
+  final CarouselController carouselController = CarouselController();
+  HorizontalCardView({
     super.key,
     required this.data,
   });
@@ -43,17 +42,57 @@ class HorizontalCardView extends StatelessWidget {
           ),
           SizedBox(
             height: 220,
-            child: ListView(
-              padding: EdgeInsets.zero,
-              shrinkWrap: true,
-              physics: PagingScrollPhysics(
-                  itemCount: 20,
-                  viewSize: MediaQuery.of(context).size.width * 0.88),
-              scrollDirection: Axis.horizontal,
+            child: CarouselView(
+              shrinkExtent: 20,
+              controller: carouselController,
+              backgroundColor: Colors.transparent,
+              itemSnapping: true,
+              onTap: (i) {
+                switch (data["items"][i]['type']) {
+                  case "playlist":
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => YoutubePlaylist(
+                                imgPath: data["items"][i]["image"],
+                                title: data["items"][i]["title"],
+                                subtitle: data["items"][i]["subtitle"],
+                                type: data["items"][i]["type"],
+                                id: data["items"][i]["id"])));
+                    break;
+                  case "chart":
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => YoutubePlaylist(
+                                imgPath: data["items"][i]["image"],
+                                title: data["items"][i]["title"],
+                                subtitle: data["items"][i]["subtitle"],
+                                type: data["items"][i]["type"],
+                                id: data["items"][i]["id"])));
+                    break;
+                  case "video":
+                    ExternalMediaImporter.ytMediaImporter(
+                            'https://youtu.be/${(data["items"][i]["id"] as String).replaceAll("youtube", "")}')
+                        .then((value) async {
+                      if (value != null) {
+                        await context
+                            .read<BloomeePlayerCubit>()
+                            .bloomeePlayer
+                            .addQueueItem(value, doPlay: true);
+                      }
+                    });
+                    break;
+                  default:
+                  // Navigator.pushNamed(context, '/album', arguments: data["items"][i]);
+                }
+              },
+              itemExtent: ((data["items"][0]['type'] == "video" ||
+                          data["items"][0]['type'] == "chart")
+                      ? 250
+                      : 150) +
+                  20,
               children: [
-                const SizedBox(
-                  width: 12,
-                ),
                 for (int i = 0; i < data['items'].length; i++)
                   SquareImgCard(
                     imgPath: data["items"][i]["image"].toString(),
@@ -71,50 +110,84 @@ class HorizontalCardView extends StatelessWidget {
                             data["items"][i]['type'] == "chart")
                         ? true
                         : false,
-                    onTap: () {
-                      switch (data["items"][i]['type']) {
-                        case "playlist":
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => YoutubePlaylist(
-                                      imgPath: data["items"][i]["image"],
-                                      title: data["items"][i]["title"],
-                                      subtitle: data["items"][i]["subtitle"],
-                                      type: data["items"][i]["type"],
-                                      id: data["items"][i]["id"])));
-                          break;
-                        case "chart":
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => YoutubePlaylist(
-                                      imgPath: data["items"][i]["image"],
-                                      title: data["items"][i]["title"],
-                                      subtitle: data["items"][i]["subtitle"],
-                                      type: data["items"][i]["type"],
-                                      id: data["items"][i]["id"])));
-                          break;
-                        case "video":
-                          ExternalMediaImporter.ytMediaImporter(
-                                  'https://youtu.be/${(data["items"][i]["id"] as String).replaceAll("youtube", "")}')
-                              .then((value) async {
-                            if (value != null) {
-                              await context
-                                  .read<BloomeePlayerCubit>()
-                                  .bloomeePlayer
-                                  .addQueueItem(value, doPlay: true);
-                            }
-                          });
-                          break;
-                        default:
-                        // Navigator.pushNamed(context, '/album', arguments: data["items"][i]);
-                      }
-                    },
                   ),
               ],
             ),
-          ),
+          )
+          // SizedBox(
+          //   height: 220,
+          //   child: ListView(
+          //     padding: EdgeInsets.zero,
+          //     shrinkWrap: true,
+          //     physics: PagingScrollPhysics(
+          //         itemCount: 20,
+          //         viewSize: MediaQuery.of(context).size.width * 0.88),
+          //     scrollDirection: Axis.horizontal,
+          //     children: [
+          //       const SizedBox(
+          //         width: 12,
+          //       ),
+          //       for (int i = 0; i < data['items'].length; i++)
+          //         SquareImgCard(
+          //           imgPath: data["items"][i]["image"].toString(),
+          //           title: data["items"][i]["title"].toString(),
+          //           subtitle: data["items"][i]["subtitle"].toString(),
+          //           isWide: (data["items"][i]['type'] == "video" ||
+          //                   data["items"][i]['type'] == "chart")
+          //               ? true
+          //               : false,
+          //           tag: (data["items"][i]['type'] == "playlist" ||
+          //                   data["items"][i]['type'] == "chart")
+          //               ? '${data["items"][i]["count"]} Tracks'
+          //               : data["items"][i]["count"].toString(),
+          //           isList: (data["items"][i]['type'] == "playlist" ||
+          //                   data["items"][i]['type'] == "chart")
+          //               ? true
+          //               : false,
+          //           onTap: () {
+          //             switch (data["items"][i]['type']) {
+          //               case "playlist":
+          //                 Navigator.push(
+          //                     context,
+          //                     MaterialPageRoute(
+          //                         builder: (context) => YoutubePlaylist(
+          //                             imgPath: data["items"][i]["image"],
+          //                             title: data["items"][i]["title"],
+          //                             subtitle: data["items"][i]["subtitle"],
+          //                             type: data["items"][i]["type"],
+          //                             id: data["items"][i]["id"])));
+          //                 break;
+          //               case "chart":
+          //                 Navigator.push(
+          //                     context,
+          //                     MaterialPageRoute(
+          //                         builder: (context) => YoutubePlaylist(
+          //                             imgPath: data["items"][i]["image"],
+          //                             title: data["items"][i]["title"],
+          //                             subtitle: data["items"][i]["subtitle"],
+          //                             type: data["items"][i]["type"],
+          //                             id: data["items"][i]["id"])));
+          //                 break;
+          //               case "video":
+          //                 ExternalMediaImporter.ytMediaImporter(
+          //                         'https://youtu.be/${(data["items"][i]["id"] as String).replaceAll("youtube", "")}')
+          //                     .then((value) async {
+          //                   if (value != null) {
+          //                     await context
+          //                         .read<BloomeePlayerCubit>()
+          //                         .bloomeePlayer
+          //                         .addQueueItem(value, doPlay: true);
+          //                   }
+          //                 });
+          //                 break;
+          //               default:
+          //               // Navigator.pushNamed(context, '/album', arguments: data["items"][i]);
+          //             }
+          //           },
+          //         ),
+          //     ],
+          //   ),
+          // ),
         ],
       ),
     );
--- lib/screens/widgets/square_card.dart
+++ lib/screens/widgets/square_card.dart
@@ -25,132 +25,115 @@ class SquareImgCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (onTap != null) onTap!();
-      },
-      child: InkWell(
-        splashColor: Default_Theme.accentColor2.withOpacity(0.15),
-        highlightColor: Default_Theme.accentColor1.withOpacity(0.12),
-        borderRadius: BorderRadius.circular(15),
-        onTap: () {
-          if (onTap != null) onTap!();
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            width: isWide ? 250 : 150,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Stack(children: [
-                    SizedBox(
-                      height: 150,
-                      width: isWide ? 250 : 150,
-                      child: LoadImageCached(
-                        imageUrl: imgPath,
-                      ),
-                    ),
-                    Visibility(
-                      visible: tag != null,
-                      child: Positioned(
-                        top: 0,
-                        right: 0,
-                        child: Container(
-                          padding: const EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            color: Default_Theme.accentColor2.withOpacity(0.95),
-                            borderRadius: const BorderRadius.only(
-                              bottomLeft: Radius.circular(15),
-                            ),
-                            // border: Border.all(
-                            //   color: Default_Theme.primaryColor1,
-                            //   width: 1.5,
-                            // ),
-                          ),
-                          child: isList
-                              ? Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    const Padding(
-                                      padding: EdgeInsets.only(right: 5),
-                                      child: Icon(
-                                        MingCute.playlist_2_line,
-                                        size: 18,
-                                        color: Default_Theme.primaryColor2,
-                                      ),
-                                    ),
-                                    Text(
-                                      "$tag",
-                                      style: const TextStyle(
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.bold,
-                                              color:
-                                                  Default_Theme.primaryColor2)
-                                          .merge(
-                                              Default_Theme.secondoryTextStyle),
-                                    ),
-                                  ],
-                                )
-                              : Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    const Padding(
-                                      padding: EdgeInsets.only(right: 5),
-                                      child: Icon(
-                                        MingCute.eye_2_line,
-                                        size: 18,
-                                        color: Default_Theme.primaryColor2,
-                                      ),
-                                    ),
-                                    Text(
-                                      "$tag",
-                                      style: const TextStyle(
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.bold,
-                                              color:
-                                                  Default_Theme.primaryColor2)
-                                          .merge(
-                                              Default_Theme.secondoryTextStyle),
-                                    ),
-                                  ],
-                                ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: isWide ? 250 : 150,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Stack(children: [
+                SizedBox(
+                  height: 150,
+                  width: isWide ? 250 : 150,
+                  child: LoadImageCached(
+                    imageUrl: imgPath,
+                  ),
+                ),
+                Visibility(
+                  visible: tag != null,
+                  child: Positioned(
+                    top: 0,
+                    right: 0,
+                    child: Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Default_Theme.accentColor2.withOpacity(0.95),
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(15),
                         ),
+                        // border: Border.all(
+                        //   color: Default_Theme.primaryColor1,
+                        //   width: 1.5,
+                        // ),
                       ),
+                      child: isList
+                          ? Row(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    MingCute.playlist_2_line,
+                                    size: 18,
+                                    color: Default_Theme.primaryColor2,
+                                  ),
+                                ),
+                                Text(
+                                  "$tag",
+                                  style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                          color: Default_Theme.primaryColor2)
+                                      .merge(Default_Theme.secondoryTextStyle),
+                                ),
+                              ],
+                            )
+                          : Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    MingCute.eye_2_line,
+                                    size: 18,
+                                    color: Default_Theme.primaryColor2,
+                                  ),
+                                ),
+                                Text(
+                                  "$tag",
+                                  style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                          color: Default_Theme.primaryColor2)
+                                      .merge(Default_Theme.secondoryTextStyle),
+                                ),
+                              ],
+                            ),
                     ),
-                  ]),
+                  ),
                 ),
-                Text(
-                  title,
-                  textAlign: TextAlign.center,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold,
-                          color: Default_Theme.primaryColor1)
-                      .merge(Default_Theme.secondoryTextStyle),
-                ),
-                Text(
-                  subtitle,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                          fontSize: 11,
-                          fontWeight: FontWeight.bold,
-                          color: Default_Theme.primaryColor2.withOpacity(0.8))
-                      .merge(Default_Theme.secondoryTextStyle),
-                ),
-              ],
+              ]),
             ),
-          ),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: Default_Theme.primaryColor1)
+                  .merge(Default_Theme.secondoryTextStyle),
+            ),
+            Text(
+              subtitle,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                      fontSize: 11,
+                      fontWeight: FontWeight.bold,
+                      color: Default_Theme.primaryColor2.withOpacity(0.8))
+                  .merge(Default_Theme.secondoryTextStyle),
+            ),
+          ],
         ),
       ),
     );
