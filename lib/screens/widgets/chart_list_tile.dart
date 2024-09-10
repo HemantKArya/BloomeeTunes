1f623731f4e0e8d09609402c95410029b9d52d53
Hemant KArya <65885023+HemantKArya@users.noreply.github.com>
2024-09-10 18:20:46 +0530

    chartlisttile onclick -getmusic and play [enhancement]

--- lib/screens/widgets/chart_list_tile.dart
+++ lib/screens/widgets/chart_list_tile.dart
@@ -1,10 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:developer';
-
+import 'package:Bloomee/blocs/mediaPlayer/bloomee_player_cubit.dart';
+import 'package:Bloomee/model/songModel.dart';
+import 'package:Bloomee/repository/MixedAPI/mixed_api.dart';
+import 'package:Bloomee/screens/widgets/snackbar.dart';
 import 'package:Bloomee/utils/imgurl_formator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-
 import 'package:Bloomee/routes_and_consts/global_str_consts.dart';
 import 'package:Bloomee/utils/load_Image.dart';
 
@@ -29,13 +32,41 @@ class ChartListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
+      onTap: () async {
         log("imgUrl: $imgUrl", name: "ChartListTile");
         if (onTap != null) {
           onTap!();
         } else {
+          SnackbarService.showMessage(
+            "Loading media...",
+            loading: true,
+          );
+          MediaItemModel? mediaItem;
+          try {
+            mediaItem =
+                await MixedAPI().getYtTrackByMeta("$title $subtitle".trim());
+            if (mediaItem != null) {
+              SnackbarService.showMessage(
+                "Media loaded.",
+                loading: false,
+                duration: const Duration(seconds: 1),
+              );
+              context
+                  .read<BloomeePlayerCubit>()
+                  .bloomeePlayer
+                  .addQueueItem(mediaItem, single: true, doPlay: true);
+              return;
+            }
+          } catch (e) {
+            log(e.toString(), name: "ChartListTile");
+          }
           context.push(
               "/${GlobalStrConsts.searchScreen}?query=$title by $subtitle");
+          SnackbarService.showMessage(
+            "Can't find media. Searching...",
+            loading: false,
+            duration: const Duration(seconds: 1),
+          );
         }
       },
       child: SizedBox(
