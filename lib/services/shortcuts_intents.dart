2045f2b89ceeaaf905b66bfc079803d1c064091c
Hemant KArya <65885023+HemantKArya@users.noreply.github.com>
2024-09-21 20:01:48 +0530

    shortcuts intent init

---
+++ lib/services/shortcuts_intents.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class PlayPauseIntent extends Intent {
+  const PlayPauseIntent();
+}
+
+class NextIntent extends Intent {
+  const NextIntent();
+}
+
+class PreviousIntent extends Intent {
+  const PreviousIntent();
+}
+
+class ShuffleIntent extends Intent {
+  const ShuffleIntent();
+}
+
+class RepeatIntent extends Intent {
+  const RepeatIntent();
+}
+
+class LikeIntent extends Intent {
+  const LikeIntent();
+}
+
+class NSecForwardIntent extends Intent {
+  const NSecForwardIntent();
+}
+
+class NSecBackwardIntent extends Intent {
+  const NSecBackwardIntent();
+}
+
+class VolumeUpIntent extends Intent {
+  const VolumeUpIntent();
+}
+
+class VolumeDownIntent extends Intent {
+  const VolumeDownIntent();
+}
+
+class MuteIntent extends Intent {
+  const MuteIntent();
+}
+
+class LoopSingleIntent extends Intent {
+  const LoopSingleIntent();
+}
+
+class LoopPlaylistIntent extends Intent {
+  const LoopPlaylistIntent();
+}
+
+class LoopOffIntent extends Intent {
+  const LoopOffIntent();
+}
+
+class TimerIntent extends Intent {
+  const TimerIntent();
+}
+
+class BackIntent extends Intent {
+  const BackIntent();
+}
