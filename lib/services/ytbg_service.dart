3304d096bcb3b8c4bdfc82605d69197a43c3c4cc
Hemant KArya <65885023+HemantKArya@users.noreply.github.com>
2024-12-23 19:44:24 +0530

    ytbg with cancelable operation

--- lib/services/ytbg_service.dart
+++ lib/services/ytbg_service.dart
@@ -1,12 +1,15 @@
 import 'dart:developer';
 import 'dart:isolate';
-
+import 'package:async/async.dart';
 import 'package:Bloomee/repository/Youtube/youtube_api.dart';
 
 Future<void> ytbgIsolate(List<dynamic> opts) async {
   final String appDocPath = opts[0] as String;
   final String appSuppPath = opts[1] as String;
   final SendPort port = opts[2] as SendPort;
+
+  CancelableOperation<Map?> canOprn =
+      CancelableOperation.fromFuture(Future.value(null));
 
   final ReceivePort receivePort = ReceivePort();
   port.send(receivePort.sendPort);
@@ -26,17 +29,26 @@ Future<void> ytbgIsolate(List<dynamic> opts) async {
       }*/
       if (data is Map) {
         var time = DateTime.now().millisecondsSinceEpoch;
-        Map? test = await yt.refreshLink(data["id"], quality: 'Low');
+        // Map? refreshedToken = await yt.refreshLink(data["id"], quality: 'Low');
+        await canOprn.cancel();
+        canOprn = CancelableOperation.fromFuture(
+          yt.refreshLink(data["id"], quality: data["quality"]),
+          onCancel: () {
+            log("Operation Cancelled-${data['id']}", name: "IsolateBG");
+          },
+        );
+
+        Map? refreshedToken = await canOprn.value;
 
         var time2 = DateTime.now().millisecondsSinceEpoch;
         log("Time taken: ${time2 - time}ms", name: "IsolateBG");
-        if (test != null) {
+        if (refreshedToken != null) {
           port.send(
             {
               "mediaId": data["mediaId"],
               "id": data["id"],
               "quality": data["quality"],
-              "link": test["url"],
+              "link": refreshedToken["url"],
             },
           );
         } else {
