ba08cd0ca5ccb770c37e721a44413768c2fe0cac
Hemant KArya <65885023+HemantKArya@users.noreply.github.com>
2025-03-01 23:23:48 +0530

    librepay added in about section

--- lib/screens/screen/home_views/setting_views/about.dart
+++ lib/screens/screen/home_views/setting_views/about.dart
@@ -25,94 +25,144 @@ class About extends StatelessWidget {
               .merge(Default_Theme.secondoryTextStyle),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Bloomee🌸\nAn Open Cross-Platform Music Player\nby",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Default_Theme.accentColor2,
-                  fontSize: 25,
-                  fontFamily: 'Unageo',
-                  fontWeight: FontWeight.bold),
-            ),
-            const GradientText(
-                text: Text(
-                  "@iamhemantindia",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Default_Theme.accentColor2,
-                      fontSize: 30,
-                      fontFamily: 'Unageo',
-                      fontWeight: FontWeight.bold),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
                 ),
-                gradient: LinearGradient(colors: [
-                  Default_Theme.accentColor1,
-                  Color.fromARGB(255, 224, 14, 192)
-                ])),
-            const Text("Crafting symphonies in code.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Default_Theme.accentColor2,
-                    fontSize: 15,
-                    fontFamily: 'Unageo',
-                    fontWeight: FontWeight.bold)),
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 20, bottom: 10, right: 10, left: 10),
-              child: FutureBuilder(
-                future: PackageInfo.fromPlatform(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    final PackageInfo packageInfo =
-                        snapshot.data as PackageInfo;
-                    return Text(
-                      "v${packageInfo.version}+${int.parse(packageInfo.buildNumber) % 1000}",
-                      style: TextStyle(
-                        color: Default_Theme.primaryColor2.withOpacity(0.5),
-                        fontSize: 15,
-                        fontFamily: 'Unageo',
+                child: IntrinsicHeight(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Bloomee🌸\nAn Open Cross-Platform Music Player\nby",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Default_Theme.accentColor2,
+                            fontSize: 25,
+                            fontFamily: 'Unageo',
+                            fontWeight: FontWeight.bold),
                       ),
-                    );
-                  } else {
-                    return const SizedBox();
-                  }
-                },
+                      const GradientText(
+                          text: Text(
+                            "@iamhemantindia",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Default_Theme.accentColor2,
+                                fontSize: 30,
+                                fontFamily: 'Unageo',
+                                fontWeight: FontWeight.bold),
+                          ),
+                          gradient: LinearGradient(colors: [
+                            Default_Theme.accentColor1,
+                            Color.fromARGB(255, 224, 14, 192)
+                          ])),
+                      const Text("Crafting symphonies in code.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Default_Theme.accentColor2,
+                              fontSize: 15,
+                              fontFamily: 'Unageo',
+                              fontWeight: FontWeight.bold)),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20, bottom: 10, right: 10, left: 10),
+                        child: FutureBuilder(
+                          future: PackageInfo.fromPlatform(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              final PackageInfo packageInfo =
+                                  snapshot.data as PackageInfo;
+                              return Text(
+                                "v${packageInfo.version}+${int.parse(packageInfo.buildNumber) % 1000}",
+                                style: TextStyle(
+                                  color: Default_Theme.primaryColor2
+                                      .withOpacity(0.5),
+                                  fontSize: 15,
+                                  fontFamily: 'Unageo',
+                                ),
+                              );
+                            } else {
+                              return const SizedBox();
+                            }
+                          },
+                        ),
+                      ),
+                      Wrap(
+                        alignment: WrapAlignment.center,
+                        spacing: 10,
+                        runSpacing: 10,
+                        children: [
+                          ElevatedButton(
+                              onPressed: () {
+                                launchUrl(
+                                    Uri.parse(
+                                        "https://github.com/HemantKArya/BloomeeTunes"),
+                                    mode: LaunchMode.externalApplication);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Default_Theme.accentColor2,
+                              ),
+                              child: const Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(right: 5),
+                                    child: Icon(MingCute.github_fill,
+                                        color: Default_Theme.primaryColor2),
+                                  ),
+                                  Text(
+                                    "Github",
+                                    style: TextStyle(
+                                        color: Default_Theme.primaryColor1,
+                                        fontSize: 15,
+                                        fontFamily: 'Unageo',
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              )),
+                          ElevatedButton(
+                              onPressed: () {
+                                launchUrl(
+                                    Uri.parse(
+                                        "https://liberapay.com/hemantkarya"),
+                                    mode: LaunchMode.externalApplication);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.yellow,
+                              ),
+                              child: const Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(right: 5),
+                                    child: Icon(MingCute.paypal_fill,
+                                        color: Colors.black),
+                                  ),
+                                  Text(
+                                    "Buy me a coffee",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15,
+                                        fontFamily: 'Unageo',
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              )),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-            ElevatedButton(
-                onPressed: () {
-                  launchUrl(
-                      Uri.parse("https://github.com/HemantKArya/BloomeeTunes"),
-                      mode: LaunchMode.externalApplication);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Default_Theme.accentColor2,
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: 5),
-                      child: Icon(MingCute.github_fill,
-                          color: Default_Theme.primaryColor2),
-                    ),
-                    Text(
-                      "Contribute on Github",
-                      style: TextStyle(
-                          color: Default_Theme.primaryColor1,
-                          fontSize: 15,
-                          fontFamily: 'Unageo',
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                )),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
