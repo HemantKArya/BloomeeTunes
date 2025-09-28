a2c98b7d8418ef29b940c9c86233e1088f776fe9
Hemant KArya <iamhemantindia@protonmail.com>
2025-09-28 23:44:57 +0530

    connectivity fix after upgrade

--- lib/blocs/internet_connectivity/cubit/connectivity_cubit.dart
+++ lib/blocs/internet_connectivity/cubit/connectivity_cubit.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 import 'package:bloc/bloc.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 part 'connectivity_state.dart';
@@ -7,10 +8,16 @@ class ConnectivityCubit extends Cubit<ConnectivityState> {
   StreamSubscription<List<ConnectivityResult>>? _subscription;
   ConnectivityCubit() : super(ConnectivityState.disconnected) {
     _subscription = Connectivity().onConnectivityChanged.listen((event) {
-      if (event.isEmpty || event.contains(ConnectivityResult.none)) {
-        emit(ConnectivityState.disconnected);
-      } else {
+      if (event.contains(ConnectivityResult.wifi) ||
+          event.contains(ConnectivityResult.mobile) ||
+          event.contains(ConnectivityResult.ethernet) ||
+          event.contains(ConnectivityResult.bluetooth) ||
+          event.contains(ConnectivityResult.vpn)) {
         emit(ConnectivityState.connected);
+        log('Connected to network: $event', name: 'ConnectivityCubit');
+      } else {
+        emit(ConnectivityState.disconnected);
+        log('Disconnected from network: $event', name: 'ConnectivityCubit');
       }
     });
   }
