b5cbacac49af800b9b1b9583629b1c17c5b62ba9
Hemant KArya <iamhemantindia@protonmail.com>
2025-11-30 13:22:38 +0530

    custom animated list

---
+++ lib/screens/widgets/animated_list_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+/// Reusable animated list item with fade-in and slide-up effect
+class AnimatedListItem extends StatelessWidget {
+  final int index;
+  final Widget child;
+
+  const AnimatedListItem({
+    super.key,
+    required this.index,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+      tween: Tween(begin: 0.0, end: 1.0),
+      duration: Duration(milliseconds: 250 + (index * 30).clamp(0, 200)),
+      curve: Curves.easeOutQuart,
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value,
+          child: Transform.translate(
+            offset: Offset(0, 16 * (1 - value)),
+            child: child,
+          ),
+        );
+      },
+      child: child,
+    );
+  }
+}
